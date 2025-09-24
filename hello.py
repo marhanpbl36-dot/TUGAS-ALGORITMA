84b0f08b6653085e550216c0b6c77211d4904d53
marhanpbl36-dot <marhanpbl36@gmail.com>
2025-09-24 10:21:18 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print("hello nama farhan muhtarom")
+print("Hobi saya,lari,main voli,dan nonton film seperti film titanic")
+print("saya ingin menjadi pengusaha")
+
