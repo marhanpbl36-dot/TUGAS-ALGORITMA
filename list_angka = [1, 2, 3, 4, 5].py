b327150d7b3be165738aeac87ecda722cfbf5f5e
marhanpbl36-dot <marhanpbl36@gmail.com>
2025-10-08 11:19:01 +0700

    Add files via upload

---
+++ list_angka = [1, 2, 3, 4, 5].py
@@ -0,0 +1,8 @@
+list_angka = [1, 2, 3, 4, 5]
+print(list_angka)
+
+# hapus data angka dibelakang
+angka_yang_terhapus = list_angka.remove(2)
+
+print('angka yang terhapus:', angka_yang_terhapus)
+print(list_angka)
