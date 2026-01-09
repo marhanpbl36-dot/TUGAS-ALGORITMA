e60909237061e1e9fc2502542a60b2900f2d19b1
marhanpbl36-dot <marhanpbl36@gmail.com>
2026-01-09 13:46:57 +0700

    Add files via upload

---
+++ nomor 1.py
@@ -0,0 +1,12 @@
+nama = input("farhan: ")
+usia = input("18: ")
+alamat = input("pangkal buluh : ")
+hobi = input("Menonton film: ")
+
+print(f"Nama    : (farhan)")
+print(f"Usia    : (18) tahun")
+print(f"Alamat  : (pangkal buluh)")
+print(f"Hobi    : (Menonton film)")
+
+# Menjalankan fungsi utama
+# tampilkan_biodata()
